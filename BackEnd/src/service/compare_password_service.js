ba92a6b2e8814572d8670b42a5e02d35675394cb
RafaelBlaske <rafael.blaske@sou.unifeob.edu.br>
2025-05-29 10:45:10 -0300

    Delete BackEnd directory

--- BackEnd/src/service/compare_password_service.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs')
-
-function comparePasswordService(password, user_password){
-
-    let isPassword = bcrypt.compareSync(password, user_password)
-    return isPassword
-}
-
-module.exports = comparePasswordService
